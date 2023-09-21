dc61d34a006642f297eac71f6069bba8dc80d269
Jhon <jhon.rosa@rede.ulbra.br>
2023-09-21 10:04:15 -0300

    Initial Commit

---
+++ xcode e github sem o terminal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xcode e github sem o terminal
+//
+//  Created by Jhon Rosa on 21/09/23.
+//
+
+import SwiftUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
